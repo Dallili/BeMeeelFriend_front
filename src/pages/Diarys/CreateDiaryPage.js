5cdcb1211812717750f183ffcab9478d3ac58c49
rwaeng <rwaeng0421@gmail.com>
2024-04-15 16:36:48 +0900

    build 오류 수정

--- src/pages/Diarys/CreateDiaryPage.js
+++ src/pages/Diarys/CreateDiaryPage.js
@@ -3,14 +3,12 @@ import {Outlet} from "react-router-dom";
 import {useState} from "react";
 
 const CreateDiaryPage = () => {
-    const [newDiaryInfo, setNewDiaryInfo] = useState("");
-    const [invite, setInvite] = useState("");
     const [color, setColor] = useState("");
 
     return (
         <div className="createDiary">
             <Header text="새로운 교환일기 생성" type="back"/>
-            <Outlet context={{setNewDiaryInfo, setInvite, setColor, setKeyword, color}}/>
+            <Outlet context={{setColor, color}}/>
         </div>
     );
 };
