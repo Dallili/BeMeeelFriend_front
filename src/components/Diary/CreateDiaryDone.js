41bb29100100d89f7a135ed78f7a117273bccb8c
rwaeng <rwaeng0421@gmail.com>
2024-04-15 15:49:04 +0900

    로그인, 다이어리 생성 api 수정

--- src/components/Diary/CreateDiaryDone.js
+++ src/components/Diary/CreateDiaryDone.js
@@ -3,30 +3,12 @@ import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {getMatchingCode, patchMatchingCode} from "../../api/matching";
 
-const CreateDiaryDone = ({who, stranger, diaryID, color}) => {
-    const [invitationCode, setInvitationCode] = useState("3fa85f64-5717-4562-b3fc-2c963f66afa6");
-
-    //유저 초대코드 받아와서 설정
-    // const myInvitationCode = "AFD#@RSFDJSFL@LFDS";
-    let myCode;
-    myCode = 111;
-    // useEffect(() => {
-    //     myCode = async () => {
-    //         await getMatchingCode(diaryID, {
-    //             userID: sessionStorage.getItem("userID"),
-    //             color: color
-    //         });
-    //     }
-    // }, []);
-
+const CreateDiaryDone = ({who, stranger, diaryID, code}) => {
     const goMain = () => window.location.replace("/");
 
-
-
     const handleCopy = () => {
-        navigator.clipboard.writeText(myCode);
+        navigator.clipboard.writeText(code);
     }
-
 
     return (
         <div className="createDiaryDone">
@@ -44,7 +26,7 @@ const CreateDiaryDone = ({who, stranger, diaryID, color}) => {
                 ): (
                     <>
                         <div className="done_explains">
-                            <div className="done_explain">일기장이 다 만들어졌어요!</div>
+                            <div className="done_explain">{diaryID}님의 일기장이 다 만들어졌어요!</div>
                             <div className="done_explain">만들어진 일기장을 친구와 공유해보세요</div>
                         </div>
                         <div className="img_box">
@@ -55,7 +37,7 @@ const CreateDiaryDone = ({who, stranger, diaryID, color}) => {
                                 <div className="invitation_explain">
                                     <div className="invitation_title">친구를 <span style={{color:"#227573", textDecoration:"#FC715F 5px underline"}}>초대하기</span> 위해서,</div>
                                     <div className="invitation_text">아래의 초대 코드를 친구에게 공유해주세요.</div>
-                                    <input className="invitation_code" disabled="true" value={myCode} />
+                                    <input className="invitation_code" disabled="true" value={code} />
                                     <button className="invitation_btn" onClick={handleCopy}>복사</button>
                                 </div>
                             ):(
