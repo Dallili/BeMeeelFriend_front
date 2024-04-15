64d49e99454c485034b7fb5d26ff22aec006c1dd
rwaeng <rwaeng0421@gmail.com>
2024-04-15 18:13:29 +0900

    연결 api 수정

--- src/components/Diary/DiaryColor.js
+++ src/components/Diary/DiaryColor.js
@@ -67,7 +67,7 @@ const DiaryColor = () => {
     const [code, setCode] = useState("");
 
     const createNewDiary = async () => {
-        const succeed = await createDiary(currentArr.substring(1));
+        const succeed = await createDiary(currentArr);
         const user = await getUserInfo();
 
         if(succeed !== "fail"){
